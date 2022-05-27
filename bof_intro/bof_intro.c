a79d5cbd8ddbd5623af8b457bf44779d7ee1fbbf
Akshith Gunasekaran <akshith.573@gmail.com>
2022-05-27 16:23:24 -0700

    x

--- bof_intro/bof_intro.c
+++ bof_intro/bof_intro.c
@@ -3,7 +3,8 @@
 int main() {
     int secret = 0xdeadbeef;
     char name[100] = {0};
-    read(0, name, 1000);
+    printf("Hi, Whats your name!\n");
+    read(0, name, 0x100);
     printf("secret: %x\n", secret);
     if (secret == 0x1337) {
         puts("Wow! Here's a secret. cs370_flag{-----------}");
