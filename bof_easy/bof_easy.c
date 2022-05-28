cfe3dc04afcdc55877c8b0fde659532de5d02464
Akshith Gunasekaran <akshith.573@gmail.com>
2022-05-28 01:06:40 -0700

    x

--- bof_easy/bof_easy.c
+++ bof_easy/bof_easy.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
 void get_flag() {
-    puts("Not bad. cs370_flag{------------}.");
+    printf("Not bad. cs370_flag{------------}.\n");
+}
+
+void say_hi() {
+    int n00b = 0x00;
+    char name[100];
+    printf("Hi, What did you say your name was again?\n");
+    read(0, name, 0x100);
+
+    if (n00b==0x00) {
+        printf("Hi n00b!\n");
+    } else {
+        printf("Hi %s\n", name);
+    }
 }
 
 int main() {
-    int n00b = 0x0;
-    char name[100] = {0};
-    printf("Hi, What did you say your name was again?\n");
-    read(0, name, 0x100);
-    printf("Hi %s\n", name);
+    say_hi();
     return 0;
 }
